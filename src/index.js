d62b900b015ec68fedc5783ac00520bdfe77a19c
liuhuan <liuhuan@133.cn>
2017-05-18 10:16:57 +0800

    hoc

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-// import 'babel-polyfill'
-// import React from 'react';
-// import { render } from 'react-dom';
-//
-//
-//
-//
-// render(
-//  <App />,
-//  document.getElementById( 'root' )
-// );
-
-
 import 'babel-polyfill'
 import React from 'react'
-import { render } from 'react-dom'
-import { Router, match, browserHistory } from 'react-router'
-// import { Provider } from 'react-redux'
+import {render} from 'react-dom'
+import {Router, match, browserHistory} from 'react-router'
 import routes from './routes'
-// import configureStore from './store/configureStore'
 
-// const store = configureStore()
-
-match( { history: browserHistory, routes }, ( error, redirectLocation, renderProps ) => {
-  render(
-  //  <Provider store = {store} >
-     <Router {...renderProps}/>,
-  //  </Provider>,
-   document.getElementById( 'root' )
-  )
-} )
+match({
+    history: browserHistory,
+    routes
+}, (error, redirectLocation, renderProps) => {
+    render(
+        <Router {...renderProps}/>, document.getElementById('root'))
+})
